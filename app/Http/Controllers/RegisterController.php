26121c31bb12868a71ddd575201291a7ad14c2eb
diego19g <diego19rm@gmail.com>
2021-05-11 20:21:55 +0200

    registro

--- app/Http/Controllers/RegisterController.php
+++ app/Http/Controllers/RegisterController.php
@@ -3,10 +3,19 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use App\Models\User;
 
 class RegisterController extends Controller
 {
-    public function RegistrarUsuario(){
+    public function RegistrarUsuario(Request $request){
+        $user=new User();
+
+        $user->name=$request->nombre;
+        $user->surname=$request->apellido;
+        $user->email=$request->email;
+        $user->password=$request->password;
+
+        $user->save();
 
     }
 }
--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -19,6 +19,8 @@ class CreateUsersTable extends Migration
             $table->string('surname');
             $table->string('email')->unique();  
             $table->string('password');
+            $table->rememberToken();
+            $table->timestamps();
         });
     }
 
