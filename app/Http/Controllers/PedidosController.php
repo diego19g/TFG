ec3200425769ec61402f865e39aead17c26043e0
diego19g <diego19rm@gmail.com>
2021-05-17 21:34:08 +0200

    tabla_pedido

--- app/Http/Controllers/PedidosController.php
+++ app/Http/Controllers/PedidosController.php
@@ -10,7 +10,7 @@ class PedidosController extends Controller
     public function MostrarPedidos(){
         $email=session('email');
         $pedidos=DB::table('pedidos')->where(['email'=>$email])->orderByDesc('created_at')->get();
-        $num = DB::table('pedidos')->distinct()->select('numero_pedido')->where(['email'=>$email])->orderByDesc('created_at')->get();
+        $num = DB::table('pedidos')->distinct()->select('numero_pedido','created_at','estado')->where(['email'=>$email])->orderByDesc('created_at')->get();
 
         return view("cliente.pedidos_cliente")->with(['pedidos'=>$pedidos,'num'=>$num]);
     }
@@ -18,7 +18,7 @@ class PedidosController extends Controller
     public function TotalPedidos(){
         $email=session('email');
         $pedidos=DB::table('pedidos')->where(['email'=>$email])->orderByDesc('created_at')->get();
-        $num = DB::table('pedidos')->distinct()->select('numero_pedido')->where(['email'=>$email])->orderByDesc('created_at')->get();
+        $num = DB::table('pedidos')->distinct()->select('numero_pedido','created_at','estado')->where(['email'=>$email])->orderByDesc('created_at')->get();
 
         return view("cliente.pedidos_cliente")->with(['pedidos'=>$pedidos,'num'=>$num]);
     }
--- resources/views/cliente/pedidos_cliente.blade.php
+++ resources/views/cliente/pedidos_cliente.blade.php
@@ -1,19 +1,18 @@
 @extends('cliente.plantilla_cliente')
 
 @section('content')
-
-<h2 style="text-align:center;">Pedidos</h2>
-      <div class="table-responsive">
-      @foreach($num as $numero)
-      <h3>ID Pedido: {{$numero->numero_pedido}}</h3>
+<section class="flotante" style="text-align:center;position:absolute;left:40%;">
+<h1 style="text-align:center;">Pedidos</h1>
+<br><hr><br>
+@foreach($num as $numero)
+      <div class="table-responsive tabla_pedido">    
+      <h4>ID Pedido: {{$numero->numero_pedido}}</h4>      
         <table class="table table-striped table-sm fondo_tabla">
           <thead>
             <tr>              
               <th>Elemento</th>
               <th>Precio</th>
               <th>Cantidad</th>
-              <th>Estado</th>
-              <th>Fecha</th>
             </tr>
           </thead>
           <tbody>          
@@ -23,14 +22,17 @@
               <td>{{ $item->name }}</td>
               <td>{{ $item->price }}</td>
               <td>{{ $item->quantity }}</td>
-              <td>{{ $item->estado }}</td>
-              <td>{{ $item->created_at }}</td>
             </tr>
             @endif
             @endforeach
           
           </tbody>
         </table>
-        @endforeach
+        <h4>Realizado: {{$numero->created_at}}</h4>
+        <h4>Estado: {{$numero->estado}}</h4>                    
       </div>
+      <br><hr><br>
+      @endforeach
+</section>
+
 @endsection
