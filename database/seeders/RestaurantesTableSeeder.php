ed0e254d9dc1caebf53fbb72b6c2059e43429f2e
diego19g <diego19rm@gmail.com>
2021-05-17 22:59:55 +0200

    restaurantes

---
+++ database/seeders/RestaurantesTableSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use Illuminate\Database\Seeder;
+use App\Models\Restaurante;
+use Illuminate\Support\Facades\Hash;
+
+class RestaurantesTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        Restaurante::create([
+            'name' => 'restaurante456',
+            'password' => Hash::make('456restaurante')
+        ]);
+
+        Restaurante::create([
+            'name' => 'restaurante518',
+            'password' => Hash::make('518restaurante')
+        ]);
+    }
+}
